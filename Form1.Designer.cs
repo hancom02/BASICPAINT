2f68292d548d497b58e196d922db377fb0d34eeb
pn3005 <22520939@gm.uit.edu.vn>
2023-12-18 11:05:03 +0700

    add MouseMove and Shapes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -680,6 +680,7 @@
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -203,6 +203,52 @@ namespace BASICPAINT
             cY = e.Y;
         }
 
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDrawing)
+                {
+                    if (curTool == TOOL.PEN)
+                    {
+                        px = e.Location;
+                        g.DrawLine(pen, px, py);
+                        py = px;
+                    }
+                    if (curTool == TOOL.ERASER)
+                    {
+                        px = e.Location;
+                        g.DrawLine(erase, px, py);
+                        py = px;
+                    }
+                }
+                if (curTool == TOOL.TRIANGLE)
+                {
+                    End = e.Location;
+                    this.Invalidate();
+                }
+                //if (curTool == TOOL.TRIANGLE)
+                //{
+                //    Point point1 = new Point() { X = startPointX, Y = startPointY + shapeHeight };
+                //    Point point2 = new Point() { X = startPointX + (shapeWidth / 2), Y = startPointY };
+                //    Point point3 = new Point() { X = startPointX + shapeWidth, Y = startPointY + shapeHeight };
+                //    g.DrawPolygon(pen, new Point[] { point1, point2, point3 });
+                //}
+            }
+            pictureBox1.Refresh();
+
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+
+            
+            
+            
+                label1.Text =  e.X + "," + e.Y;
+            
+        }
+
         private void guna2PictureBox2_Click_1(object sender, EventArgs e)
         {
 
