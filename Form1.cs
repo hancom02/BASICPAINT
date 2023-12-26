d2b904e2ab2ad1cf5dcb82e64b3a1e919550f659
pn3005 <22520939@gm.uit.edu.vn>
2023-12-26 14:41:10 +0700

    add form_exit

--- Form1.cs
+++ Form1.cs
@@ -407,6 +407,14 @@ namespace BASICPAINT
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            Form1.ActiveForm.Close();
+        }
+
+        
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
             string message = "Do you want to save currrent drawing?";
             string title = "Close Window";
             MessageBoxButtons buttons = MessageBoxButtons.YesNo;
